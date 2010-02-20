c7189930844331c4c94ce2260c372fc4ee2698a9
Ben Welsh <ben@loftbook.(none)>
2010-02-19 23:47:52 -0800

    Added a month-to-month browse feature; Added urls and views for viewing any month as a detail page; Refactored views a little.

--- geodjango_dunktank/unemployment/views.py
+++ geodjango_dunktank/unemployment/views.py
@@ -4,22 +4,55 @@ from unemployment.models import CountyByMonth
 from django.views.generic.simple import direct_to_template
 
 
-def index(request):
+def render_map(request, object_list):
     """
-    The homepage.
+    Feeds data out to our map template.
     """
     context = {}
-    
-    # Query the latest months' data
-    try:
-        object_list = CountyByMonth.unadjusted.latest()
-    except CountyByMonth.DoesNotExist:
-        raise Http404
-    
+
     # Add it to the context we'll pass out to the template
     context['object_list'] = object_list
     
     # Grab the latest month as a datetime object
     context['month'] = object_list[0].get_month_obj()
     
-    return direct_to_template(request, 'unemployment/index.html', context)
+    # If data exists for the next month, pass it along
+    next = object_list[0].get_next_month()
+    if CountyByMonth.objects.filter(year=next.year, month=next.month):
+        context['next_month'] = next
+
+    # Same for the previous month
+    prev = object_list[0].get_previous_month()
+    if CountyByMonth.objects.filter(year=prev.year, month=prev.month):
+        context['previous_month'] = prev
+    
+    return direct_to_template(request, 'unemployment/map.html', context)
+
+
+def index(request):
+    """
+    The homepage.
+    
+    Reports the latest month's data.
+    """    
+    # Query the latest months' data
+    try:
+        object_list = CountyByMonth.unadjusted.latest()
+    except CountyByMonth.DoesNotExist:
+        raise Http404
+    
+    return render_map(request, object_list)
+    
+    
+def month_detail(request, year, month):
+    """
+    Reports the data for a particular month.
+    """
+    try:
+        object_list = CountyByMonth.unadjusted.filter(year=year, month=month)
+    except:
+        raise Http404
+    if not object_list:
+        raise Http404
+    return render_map(request, object_list)
+    
--- geodjango_dunktank/urls.py
+++ geodjango_dunktank/urls.py
@@ -5,6 +5,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
 
     url(r'^$', 'unemployment.views.index', name='unemployment-index'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$',
+        'unemployment.views.month_detail', name='unemployment-month-detail'),
 
     (r'^admin/', include(admin.site.urls)),
 
